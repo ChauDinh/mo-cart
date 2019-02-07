c4269ad91f7e527cb57bc6140c48535e854329bb
ChauDinh <chaudvb95uc@gmail.com>
2019-02-07 22:36:50 +0700

    Button carousel

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,9 @@ const bannerCarousel = document.querySelector("[data-js=banner__carousel]");
 const slideOne = document.querySelector("[data-js=pic__one]");
 const slideTwo = document.querySelector("[data-js=pic__two]");
 const slideThree = document.querySelector("[data-js=pic__three]");
+const buttonOne = document.querySelector("[data-js=btn__pic-1]");
+const buttonTwo = document.querySelector("[data-js=btn__pic-2]");
+const buttonThree = document.querySelector("[data-js=btn__pic-3]");
 
 slideOne.addEventListener("click", e => {
  slideOne.style.visibility = "hidden";
@@ -24,4 +27,28 @@ slideThree.addEventListener("click", e => {
  bannerCarousel.classList.remove("show__two");
  bannerCarousel.classList.remove("show__three");
 });
+
+buttonOne.addEventListener("click", e => {
+ slideOne.style.visibility = "visible";
+ slideTwo.style.visibility = "hidden";
+ slideThree.style.visibility = "hidden";
+ bannerCarousel.classList.remove("show__two");
+ bannerCarousel.classList.remove("show__three");
+});
+
+buttonTwo.addEventListener("click", e => {
+ slideOne.style.visibility = "hidden";
+ slideTwo.style.visibility = "visible";
+ slideThree.style.visibility = "hidden";
+ bannerCarousel.classList.toggle("show__two");
+ bannerCarousel.classList.remove("show__three");
+});
+
+buttonThree.addEventListener("click", e => {
+ slideOne.style.visibility = "hidden";
+ slideTwo.style.visibility = "hidden";
+ slideThree.style.visibility = "visible";
+ bannerCarousel.classList.toggle("show__three");
+ bannerCarousel.classList.remove("show__two");
+})
 

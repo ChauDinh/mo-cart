a7ea57b904d9476681dad31720ec0d40cce6fc01
ChauDinh <chaudvb95uc@gmail.com>
2019-02-07 21:36:53 +0700

    Banner Carousel

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,27 @@
-// alert("Website hiện đang bảo trì ...")
+// Banner Carousel 
 
-console.log("Hello, world!")
+const bannerCarousel = document.querySelector("[data-js=banner__carousel]");
+const slideOne = document.querySelector("[data-js=pic__one]");
+const slideTwo = document.querySelector("[data-js=pic__two]");
+const slideThree = document.querySelector("[data-js=pic__three]");
+
+slideOne.addEventListener("click", e => {
+ slideOne.style.visibility = "hidden";
+ slideTwo.style.visibility = "visible";
+ slideThree.style.visibility = "hidden";
+ bannerCarousel.classList.toggle("show__two");
+});
+
+slideTwo.addEventListener("click", e => {
+ slideOne.style.visibility = "hidden";
+ slideTwo.style.visibility = "hidden";
+ slideThree.style.visibility = "visible";
+ bannerCarousel.classList.toggle("show__three");
+});
+
+slideThree.addEventListener("click", e => {
+ slideOne.style.visibility = "visible";
+ bannerCarousel.classList.remove("show__two");
+ bannerCarousel.classList.remove("show__three");
+});
+

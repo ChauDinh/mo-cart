664956c7cc5aba36ecf12bbcb9e9b3e6e48e604c
ChauDinh <chaudvb95uc@gmail.com>
2019-02-13 17:02:41 +0700

    Create User Validation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -15,7 +15,8 @@ module.exports.search = function(req, res) {
  });
  
  res.render('users/index', {
-  users: matchedUsers
+  users: matchedUsers,
+  values: q
  });
 };
 
@@ -34,6 +35,26 @@ module.exports.get = function(req, res) {
 
 module.exports.postCreate = function(req, res) {
  req.body.id = shortid.generate();
+ let errors = [];
+
+ // Handling errors
+ if (!req.body.name) {
+  errors.push('Name is required!');
+ }
+ 
+ if (!req.body.phone) {
+  errors.push("Phone is required!");
+ }
+
+ if (errors.length) {
+  res.render("users/create", {
+   errors: errors,
+   values: req.body
+  });
+  return;
+ }
+
+ // Push to database
  db.get("users").push(req.body).write();
  res.redirect("/users");
 };
